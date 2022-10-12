df6b4a4cc531184c5737a3fd47dfe348607c08ec
aarcher2022 <archer.antoine@gmail.com>
2022-10-12 11:24:07 +0200

    1er état de modifier profil, modif de tout sauf mdp. Reste à faire : - vérouiller pour ne pouvoir modifier que son profil depuis l'url - modif mdp - Photo à télécharger - Gestion des erreurs

---
+++ src/Form/ParticipantType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use App\Entity\Site;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ParticipantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('pseudo')
+            ->add('nom')
+            ->add('prenom')
+            ->add('telephone')
+            ->add('mail', null, ['label'=>'Email'] )
+            ->add('site', EntityType::class,
+                ['class'=>Site::class, 'choice_label'=>'nomSite', 'label'=>'Site de rattachement'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
